3f3aee9a653b4cb66f70ed90c8e64bf439f2cd9e
Basile <54018@etu.he2b.be>
2023-01-20 16:21:12 +0100

    Addition of the footer Component

--- src/Layouts/Footer/Footer.jsx
+++ src/Layouts/Footer/Footer.jsx
@@ -1,24 +1,72 @@
 // Create a footer like Facebook with function component
-function Footer(){
+function Footer() {
     return (
-        <div className="footer">
-            <div className="footer__left">
-                <p>Facebook © 2021</p>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <h4>CoursLearning</h4>
+                        <p>Le nom du site</p>
+                    </div>
+                    <div class="col-lg-2">
+                        <h4>Learn More</h4>
+                        <ul>
+                            <li>Sous point 1</li>
+                            <li>Sous point 2</li>
+                            <li>Sous point 3</li>
+                            <li>Sous point 4</li>
+                            <li>Sous point 5</li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-2">
+                        <h4>Contact Us</h4>
+                        <ul>
+                            <li>Sous point 1</li>
+                            <li>Sous point 2</li>
+                            <li>Sous point 3</li>
+                            <li>Sous point 4</li>
+                            <li>Sous point 5</li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4">
+                        <h4>Social</h4>
+                        <ul>
+                            <li>
+                                <img
+                                    src="lien_vers_image_reseau_social1"
+                                    alt="Réseau social 1"
+                                />
+                            </li>
+                            <li>
+                                <img
+                                    src="lien_vers_image_reseau_social2"
+                                    alt="Réseau social 2"
+                                />
+                            </li>
+                            <li>
+                                <img
+                                    src="lien_vers_image_reseau_social3"
+                                    alt="Réseau social 3"
+                                />
+                            </li>
+                            <li>
+                                <img
+                                    src="lien_vers_image_reseau_social4"
+                                    alt="Réseau social 4"
+                                />
+                            </li>
+                            <li>
+                                <img
+                                    src="lien_vers_image_reseau_social5"
+                                    alt="Réseau social 5"
+                                />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div className="footer__right">
-                <ul>
-                    <li>English (US)</li>
-                    <li>Privacy</li>
-                    <li>Terms</li>
-                    <li>Advertising</li>
-                    <li>Ad Choices</li>
-                    <li>Cookies</li>
-                    <li>More</li>
-                </ul>
-            </div>
-        </div>
+        </footer>
     )
 }
 
-
-export default Footer;
+export default Footer
